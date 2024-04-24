6827bfb93284b7b26345b90071419e76fc035170
smilee <smilee@users.noreply.github.com>
2024-04-24 23:03:29 +0900

    240424 add description (#16)
    * Adjust background to scroll
    
    * Give float effect to the logo

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -5,11 +5,28 @@ import BackgroundContainer from '../components/Background';
 import MainContainer from '../components/Main';
 import FooterContainer from '../components/Footer';
 
+import styled from '@emotion/styled'
+
+const LogoContainer = styled.div`
+  animation: float 4s ease-in-out infinite;
+
+  @keyframes float {
+    0%, 100% {
+        transform: translateY(0);
+    }
+    50% {
+        transform: translateY(-20px);
+    }
+  }
+`
+
 export default function Root() {
     return (
       <>
         <BackgroundContainer />
-        <img src={logo} className="logo" alt="Bubbly's Symphony: Guardians of the Depths" />
+        <LogoContainer>
+          <img src={logo} className="logo" alt="Bubbly's Symphony: Guardians of the Depths" />
+        </LogoContainer>
         <MainContainer />
         <FooterContainer />
       </>
