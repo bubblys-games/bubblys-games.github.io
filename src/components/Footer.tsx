27a878946da8afbb31d69e42c4835adaf5f7c174
Seungmin Lee <seungmin@uxplus.dev>
2024-05-17 18:40:22 +0900

    Adjust Footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export default function FooterContainer() {
   }
 
   return (
-    <footer className="box-border px-0 py-16 md:py-16 w-full text-5b3f3e">
+    <footer className="box-border px-4 py-16 md:py-16 w-full text-5b3f3e">
       <div className="mb-6">
         <Link href="https://x.com/bubbly_robot" className="text-white inline-flex mr-5 text-4xl" target="_blank"><XLogo /></Link>
         <Link href="https://www.instagram.com/bubbly_robot/" className="text-white inline-flex mr-5 text-4xl" target="_blank"><InstagramLogo /></Link>
@@ -22,7 +22,7 @@ export default function FooterContainer() {
       </div>
       <Link href="/terms" className="mr-5 text-[#eae6cf] text-md" onClick={scrollToTop}>Terms of Conduct</Link>
       <Link href="/privacy" className="mr-5 text-[#eae6cf] text-md" onClick={scrollToTop}>Privacy Policy</Link>
-      <Link href="/support" className="mr-5 text-[#eae6cf] text-md" onClick={scrollToTop}>Support</Link>
+      <Link href="/support" className="text-[#eae6cf] text-md" onClick={scrollToTop}>Support</Link>
       <p className="mt-4 text-[#5b3f3e] text-sm">&copy; {new Date().getFullYear()} Seungmin Lee. All rights reserved.</p>
       <p className="my-1 text-[#5b3f3e] text-sm">&quot;Download on the App Store&quot; and the App Store badge are trademarks of Apple Inc.</p>
       <p className="my-1 text-[#5b3f3e] text-sm">Google Play and the Google Play logo are trademarks of Google LLC.</p>
